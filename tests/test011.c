14aaaa1bb9fc6bf5cbf8d0b00ec2cb83fe6ed8b9
Rajnesh Kanwal <rkanwal@rivosinc.com>
2023-07-26 15:00:23 +0100

    Add support to handle exceptions and interrupts occuring during umode.
    Also adding a test to handle illegal instruction trap from user mode
    execution.
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

---
+++ tests/test011.c
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2023 Rivos Inc.
+//
+// SPDX-License-Identifier: LicenseRef-Rivos-Internal-Only
+
+#include "jumpstart_functions.h"
+
+int test_illegal_instruction_in_umode(void);
+
+static volatile uint32_t excep_rcvd = 0;
+
+static void test011_exception_handler(void) {
+  uint64_t reg = read_csr(sepc);
+
+  /* Just skip the illegal instruction and move to next instruction. */
+  write_csr(sepc, reg + 4);
+
+  /* Set excep_rcvd to non-zero to notify main that exception occurred. */
+  excep_rcvd = 0xabcdabcd;
+}
+
+int main(void) {
+  if (get_thread_attributes_hart_id() != 0) {
+    return 1;
+  }
+
+  if (get_thread_attributes_bookend_magic_number() !=
+      THREAD_ATTRIBUTES_BOOKEND_MAGIC_NUMBER_VALUE) {
+    return 1;
+  }
+
+  if (get_thread_attributes_current_mode() != SUPERVISOR_MODE_ENCODING) {
+    return 1;
+  }
+
+  register_trap_handler_override(SUPERVISOR_MODE_ENCODING,
+                                 SCAUSE_EC_ILLEGAL_INSTRUCTION,
+                                 (uint64_t)(&test011_exception_handler));
+
+  setup_mmu_for_supervisor_mode();
+
+  if (run_function_in_user_mode(test_illegal_instruction_in_umode) != 0) {
+    return 1;
+  }
+
+  if (excep_rcvd != 0xabcdabcd) {
+    return 1;
+  }
+
+  disable_mmu_for_supervisor_mode();
+
+  return 0;
+}
