bca193f252ef41f4f68a48e5b284864c0bcae188
Jerin Joy <joy@rivosinc.com>
2023-08-03 13:40:22 -0700

    Added MP IMSIC test
    Removed UART address from test008 memory map.

---
+++ tests/test015.c
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: 2023 Rivos Inc.
+//
+// SPDX-License-Identifier: LicenseRef-Rivos-Internal-Only
+
+#include "jumpstart_functions.h"
+#include "jumpstart_imsic.h"
+
+volatile uint64_t irq_var = 0;
+volatile uint8_t hart_imsic_enabled[MAX_NUM_HARTS_SUPPORTED] = {0};
+
+static void test008_exception_handler(void) {
+  irq_var = 1;
+  write_csr(stopei, 0);
+}
+
+int main(void) {
+  uint64_t hart_id = get_thread_attributes_hart_id_from_supervisor_mode();
+
+  if (hart_id == PRIMARY_HART_ID) {
+    uint64_t active_hart_mask = get_active_hart_mask_from_supervisor_mode();
+
+    uint8_t current_hart_id = 0;
+    while (active_hart_mask != 0) {
+      if (current_hart_id != hart_id && (active_hart_mask & 0x1)) {
+        while (hart_imsic_enabled[current_hart_id] == 0)
+          ;
+
+        send_ipi_to_supervisor_mode(current_hart_id);
+      }
+
+      active_hart_mask >>= 1;
+      ++current_hart_id;
+    }
+  } else {
+    register_trap_handler_override(SUPERVISOR_MODE_ENCODING,
+                                   SCAUSE_INT_EXTERNAL |
+                                       (1ULL << SCAUSE_INTERRUPT_BIT_LSB),
+                                   (uint64_t)(&test008_exception_handler));
+
+    imsic_init();
+
+    imsic_id_enable(IMSIC_IPI_ID);
+
+    hart_imsic_enabled[hart_id] = 1;
+    asm volatile("wfi");
+
+    if (irq_var != 1) {
+      return DIAG_FAILED;
+    }
+
+    imsic_id_disable(IMSIC_IPI_ID);
+    imsic_fini();
+  }
+
+  return DIAG_PASSED;
+}
