54b17812f321a74e495ac2ce2ce8f5541bc49b44
Atish Patra <atishp@rivosinc.com>
2023-08-16 17:15:39 -0700

    Add a new uart test with SMP configuration
    We don't have spinlocks yet. Thus, just let hart0 setup the UART and
    write to it. All other non-booting hart should return success.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

---
+++ tests/test016.c
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2023 Rivos Inc.
+//
+// SPDX-License-Identifier: LicenseRef-Rivos-Internal-Only
+
+#include "jumpstart_functions.h"
+#include "jumpstart_uart.h"
+
+int main(void) {
+  if (get_thread_attributes_bookend_magic_number_from_supervisor_mode() !=
+      THREAD_ATTRIBUTES_BOOKEND_MAGIC_NUMBER_VALUE) {
+    return DIAG_FAILED;
+  }
+
+  if (get_diag_satp_mode_from_supervisor_mode() != SATP_MODE_SV39) {
+    return DIAG_FAILED;
+  }
+
+  if (get_thread_attributes_current_mode_from_supervisor_mode() !=
+      SUPERVISOR_MODE_ENCODING) {
+    return DIAG_FAILED;
+  }
+
+  /* TODO: We don't have a spinlock. Allow only Hart0 to write to the UART */
+  uint8_t hart_id = get_thread_attributes_hart_id_from_supervisor_mode();
+  if (hart_id == 0) {
+    setup_uart();
+    puts("Hello World\n");
+    printk("Checking format specifier int %d, char %c \n", 0xc001, 'A');
+  } else if (hart_id > 3) {
+    return DIAG_FAILED;
+  }
+
+  return DIAG_PASSED;
+}
