897ac9dca4140186bc326c4015271ce5105a38be
Balaji Ravikumar <bravikumar@rivosinc.com>
2023-12-22 10:54:46 -0800

    Updated printk() to be thread safe
    updated test016 to test thread safe printk().
    
    puts() is still not thread safe.

--- src/common/uart_functions.supervisor.c
+++ src/common/uart_functions.supervisor.c
@@ -5,21 +5,24 @@
 #include "uart_functions.supervisor.h"
 #include "jumpstart_defines.h"
 #include "jumpstart_functions.h"
+#include "lock_functions.supervisor.h"
 
 #include <inttypes.h>
 #include <stdarg.h>
 #include <stdio.h>
 
-int toupper(int c);
+extern void setup_uart(void);
+extern void putch(char c);
 
+int toupper(int c);
 static int vprintk(const char *fmt, va_list args)
     __attribute__((format(printf, 1, 0)))
     __attribute__((section(".jumpstart.text.supervisor")));
 void enable_uart(void);
-static uint8_t uart_initialized = 0;
 
-extern void setup_uart(void);
-extern void putch(char c);
+static uint8_t uart_initialized = 0;
+__attribute__((
+    section(".jumpstart.data.supervisor"))) static spinlock_t uart_lock = 0;
 
 __attribute__((section(".jumpstart.text.machine"))) void enable_uart(void) {
   uart_initialized = 1;
@@ -67,9 +70,13 @@ printk(const char *fmt, ...) {
   va_list args;
   int rc;
 
+  acquire_lock(&uart_lock);
+
   va_start(args, fmt);
   rc = vprintk(fmt, args);
   va_end(args);
+
+  release_lock(&uart_lock);
 
   return rc;
 }
