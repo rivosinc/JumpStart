7277cfbd7c13585bf6ec7a173fd73981bd45acf0
Rajnesh Kanwal <rkanwal@rivosinc.com>
2024-01-22 21:44:24 +0000

    Add jumpstart_umode_fail function.
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

---
+++ tests/common/test033.c
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2023 - 2024 Rivos Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "jumpstart_functions.h"
+
+int umode_main(void) __attribute__((section(".text.user"), noreturn));
+
+int main(void) {
+  return run_function_in_user_mode((uint64_t)umode_main, 0);
+}
+
+int umode_main(void) {
+  jumpstart_umode_fail();
+}
