ec41a02754da62914eec85a5a66743efcd0b8ecf
Rajnesh Kanwal <rkanwal@rivosinc.com>
2024-01-22 21:45:44 +0000

    Add support to pass arguments to smode function.
    Co-authored-by: Jerin Joy <joy@rivosinc.com>
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

--- tests/common/test018.c
+++ tests/common/test018.c
@@ -8,6 +8,13 @@
 // Supervisor functions
 // The assembly functions are already tagged with the .text.supervisor section
 // attribute.
+uint8_t asm_check_passed_in_arguments(uint8_t a0, uint8_t a1, uint8_t a2,
+                                      uint8_t a3, uint8_t a4, uint8_t a5,
+                                      uint8_t a6);
+uint8_t c_check_passed_in_arguments(uint8_t a0, uint8_t a1, uint8_t a2,
+                                    uint8_t a3, uint8_t a4, uint8_t a5,
+                                    uint8_t a6)
+    __attribute__((section(".text.supervisor"))) __attribute__((const));
 uint8_t get_bytes_to_copy(void);
 int copy_bytes(void);
 int compare_copied_bytes(void) __attribute__((section(".text.supervisor")))
@@ -15,6 +22,33 @@ __attribute__((pure));
 
 extern uint64_t source_location;
 extern uint64_t destination_location;
+
+uint8_t c_check_passed_in_arguments(uint8_t a0, uint8_t a1, uint8_t a2,
+                                    uint8_t a3, uint8_t a4, uint8_t a5,
+                                    uint8_t a6) {
+  if (a0 != 1) {
+    return DIAG_FAILED;
+  }
+  if (a1 != 2) {
+    return DIAG_FAILED;
+  }
+  if (a2 != 3) {
+    return DIAG_FAILED;
+  }
+  if (a3 != 4) {
+    return DIAG_FAILED;
+  }
+  if (a4 != 5) {
+    return DIAG_FAILED;
+  }
+  if (a5 != 6) {
+    return DIAG_FAILED;
+  }
+  if (a6 != 7) {
+    return DIAG_FAILED;
+  }
+  return DIAG_PASSED;
+}
 
 int main(void) {
   if (get_thread_attributes_hart_id_from_machine_mode() != 0) {
@@ -27,6 +61,16 @@ int main(void) {
   }
 
   if (get_thread_attributes_current_mode_from_machine_mode() != PRV_M) {
+    return DIAG_FAILED;
+  }
+
+  if (run_function_in_supervisor_mode((uint64_t)asm_check_passed_in_arguments,
+                                      1, 2, 3, 4, 5, 6, 7) != DIAG_PASSED) {
+    return DIAG_FAILED;
+  }
+
+  if (run_function_in_supervisor_mode((uint64_t)c_check_passed_in_arguments, 1,
+                                      2, 3, 4, 5, 6, 7) != DIAG_PASSED) {
     return DIAG_FAILED;
   }
 
