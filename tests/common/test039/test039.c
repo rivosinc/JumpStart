2a3644d9d0bdffececff4935bca9367d2a750b14
Balaji Ravikumar <bravikumar@rivosinc.com>
2024-05-06 12:35:56 -0700

    fix: MP Malloc test Shouldnt over allocate.
    To prevent running out of heap space when allocating aligned addresses
    we perform a simple check to see if the worst case allocation will fit
    in the available heap space.
    
    Signed-off-by: Balaji Ravikumar <bravikumar@rivosinc.com>

--- tests/common/test039/test039.c
+++ tests/common/test039/test039.c
@@ -14,8 +14,15 @@ every hart/iteration in a allocation table.
 We expect all the pointers across harts for a given iteration to be unique.
 */
 
-#define NUM_INTERATIONS 10
-#define ALLOCS_PER_HART 16
+#define NUM_INTERATIONS     8
+#define ALLOCS_PER_HART     12
+#define HEAP_STRUCT_PADDING 16
+extern uint64_t _JUMPSTART_SMODE_HEAP_START;
+extern uint64_t _JUMPSTART_SMODE_HEAP_END;
+// Sorted in ascending order
+const uint64_t alloc_sizes[] = {8, 16, 32, 48, 64};
+const uint64_t aligns[] = {0x8, 0x10, 0x80};
+#define ARRAY_LEN(arr, type) (sizeof(arr) / sizeof(type))
 
 void *allocated[MAX_NUM_HARTS_SUPPORTED][NUM_INTERATIONS][ALLOCS_PER_HART] = {
     0};
@@ -32,17 +39,15 @@ static uint64_t allocation_entropy(uint64_t seed_hash, uint64_t hart_id,
 
 static uint64_t get_allocation_size(uint64_t hart_id, uint64_t iter,
                                     uint64_t alloc_index) {
-  const uint64_t alloc_sizes[] = {8, 16, 32, 48, 64};
   uint64_t hash = allocation_entropy(0, hart_id, iter, alloc_index);
-  return alloc_sizes[hash % (sizeof(alloc_sizes) / sizeof(uint64_t))];
+  return alloc_sizes[hash % ARRAY_LEN(alloc_sizes, uint64_t)];
 }
 
 static uint64_t get_allocation_align(uint64_t hart_id, uint64_t iter,
                                      uint64_t alloc_index) {
-  const uint64_t aligns[] = {0x8, 0x10, 0x100};
   uint64_t hash = allocation_entropy(0, hart_id, iter, alloc_index);
   hash = allocation_entropy(hash, hart_id, iter, alloc_index);
-  return aligns[hash % (sizeof(aligns) / sizeof(uint64_t))];
+  return aligns[hash % ARRAY_LEN(aligns, uint64_t)];
 }
 
 static int make_allocations(uint64_t hart_id, int iter) {
@@ -117,14 +122,14 @@ static int test_allocations(uint64_t hart_id, int iter) {
   return DIAG_PASSED;
 }
 
-static int test_malloc(uint64_t hart_id) {
+int test_malloc(uint64_t hart_id) {
   // Make sure all hart start at the same time
   sync_all_harts_from_smode();
   for (int i = 0; i < NUM_INTERATIONS; i++) {
-    if (make_allocations(hart_id, i) == DIAG_FAILED) {
+    if (make_allocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
-    if (test_allocations(hart_id, i) == DIAG_FAILED) {
+    if (test_allocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
     free_some_allocations(hart_id, i);
@@ -134,14 +139,14 @@ static int test_malloc(uint64_t hart_id) {
   return DIAG_PASSED;
 }
 
-static int test_calloc(uint64_t hart_id) {
+int test_calloc(uint64_t hart_id) {
   // Make sure all hart start at the same time
   sync_all_harts_from_smode();
   for (int i = 0; i < NUM_INTERATIONS; i++) {
-    if (make_callocations(hart_id, i) == DIAG_FAILED) {
+    if (make_callocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
-    if (test_allocations(hart_id, i) == DIAG_FAILED) {
+    if (test_allocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
     free_some_allocations(hart_id, i);
@@ -151,20 +156,35 @@ static int test_calloc(uint64_t hart_id) {
   return DIAG_PASSED;
 }
 
-static int test_memalign(uint64_t hart_id) {
+int test_memalign(uint64_t hart_id) {
   // Make sure all hart start at the same time
   sync_all_harts_from_smode();
   for (int i = 0; i < NUM_INTERATIONS; i++) {
-    if (make_aligned_allocations(hart_id, i) == DIAG_FAILED) {
+    if (make_aligned_allocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
-    if (test_allocations(hart_id, i) == DIAG_FAILED) {
+    if (test_allocations(hart_id, i) != DIAG_PASSED) {
       return DIAG_FAILED;
     }
     free_some_allocations(hart_id, i);
   }
   sync_all_harts_from_smode();
   cleanup_test(hart_id);
+  return DIAG_PASSED;
+}
+
+static int check_heap_size(void) {
+  // This check ensures that all planned allocation for the worst case will fit
+  // in available heap size.
+  const uint64_t max_heap_size = (uint64_t)&_JUMPSTART_SMODE_HEAP_END -
+                                 (uint64_t)&_JUMPSTART_SMODE_HEAP_START;
+  const uint64_t max_align = aligns[ARRAY_LEN(aligns, uint64_t) - 1];
+  const uint64_t max_alloc = alloc_sizes[ARRAY_LEN(alloc_sizes, uint64_t) - 1];
+  if (max_heap_size / max_align / ALLOCS_PER_HART / NUM_INTERATIONS /
+          MAX_NUM_HARTS_SUPPORTED <
+      (max_alloc + HEAP_STRUCT_PADDING)) {
+    return DIAG_FAILED;
+  }
   return DIAG_PASSED;
 }
 
@@ -173,13 +193,16 @@ int main(void) {
   if (hart_id > MAX_NUM_HARTS_SUPPORTED) {
     return DIAG_FAILED;
   }
-  if (test_malloc(hart_id) == DIAG_FAILED) {
+  if (check_heap_size() != DIAG_PASSED) {
     return DIAG_FAILED;
   }
-  if (test_calloc(hart_id) == DIAG_FAILED) {
+  if (test_malloc(hart_id) != DIAG_PASSED) {
     return DIAG_FAILED;
   }
-  if (test_memalign(hart_id) == DIAG_FAILED) {
+  if (test_calloc(hart_id) != DIAG_PASSED) {
+    return DIAG_FAILED;
+  }
+  if (test_memalign(hart_id) != DIAG_PASSED) {
     return DIAG_FAILED;
   }
   return DIAG_PASSED;
