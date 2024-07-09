fa9dcb70357c31e81ec0e0b9505a9a089bf28480
Jerin Joy <joy@rivosinc.com>
2024-07-08 17:20:46 -0700

    Added a LinkerScript class to keep linker script functions and checks
    Along with improved checking, we can now also merge mappings that are
    part of the same linker script section. They mappings can have different
    attributes but they will be part of the larger merged linker script
    section.
    
    Updated test010 (ELF checks) to test for the linker script section
    merging.
    
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

--- scripts/memory_management/__init__.py
+++ scripts/memory_management/__init__.py
@@ -4,6 +4,7 @@
 
 # __init__.py
 
+from .linker_script import LinkerScript
 from .memory_mapping import MemoryMapping
 from .page_size import PageSize
 from .page_tables import (
@@ -21,6 +22,7 @@ from .page_tables import (
 
 __all__ = [
     "AddressType",
+    "LinkerScript",
     "PageSize",
     "MemoryMapping",
     "PageTables",
