a5793695f035f0e5da5a14d3376326b2631262b7
Jerin Joy <joy@rivosinc.com>
2024-07-30 15:35:47 -0700

    Added test049,050 to test jumpstart_v*mode_fail()
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

---
+++ tests/common/test049/test049.c
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2023 - 2024 Rivos Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "jumpstart.h"
+
+void vumode_main(void) __attribute__((section(".text.vumode")));
+int vsmode_main(void) __attribute__((section(".text.vsmode")));
+
+int main(void) {
+  return run_function_in_vsmode((uint64_t)vsmode_main);
+}
+
+int vsmode_main(void) {
+  return run_function_in_vumode((uint64_t)vumode_main);
+}
+
+void vumode_main(void) {
+  jumpstart_vumode_fail();
+}
---
+++ tests/common/test050/test050.c
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 Rivos Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "jumpstart.h"
+
+__attribute__((const)) int main(void);
+
+void vsmode_main(void) __attribute__((section(".text.vsmode")));
+
+void vsmode_main(void) {
+  jumpstart_vsmode_fail();
+}
+
+int main(void) {
+  return run_function_in_vsmode((uint64_t)vsmode_main);
+}
