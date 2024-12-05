89218e9a75d5e1d406c669f99c04038fd44c94ac
Jerin Joy <joy@rivosinc.com>
2024-12-05 02:52:33 -0800

    Added test058 to test holes in the active_hart_mask
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

---
+++ tests/common/test058/test058.c
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2023 - 2024 Rivos Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "cpu_bits.h"
+#include "heap.smode.h"
+#include "jumpstart.h"
+
+int main(void) {
+  uint8_t hart_id = get_thread_attributes_hart_id_from_smode();
+
+  if (hart_id != 1 && hart_id != 3) {
+    return DIAG_FAILED;
+  }
+
+  if (PRIMARY_HART_ID != 1) {
+    // The hart with the lowest hart_id in the active hart mask is the primary
+    // hart.
+    return DIAG_FAILED;
+  }
+
+  return DIAG_PASSED;
+}
