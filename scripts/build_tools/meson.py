c2bc22bba619a312f87518287565d4462cdde108
Rajnesh Kanwal <rkanwal@rivosinc.com>
2025-03-03 13:49:12 +0000

    Remove target name from trace file name
    This is needed by DV infra.
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>

--- scripts/build_tools/meson.py
+++ scripts/build_tools/meson.py
@@ -95,9 +95,7 @@ class Meson:
         self.meson_options["spike_additional_arguments"] = []
 
         self.meson_options["generate_trace"] = "true"
-        self.trace_file = (
-            f"{self.meson_builddir}/{self.diag_name}.{self.diag_build_target.target}.itrace"
-        )
+        self.trace_file = f"{self.meson_builddir}/{self.diag_name}.itrace"
 
         self.meson_options["diag_target"] = self.diag_build_target.target
         if self.diag_build_target.target == "spike":
